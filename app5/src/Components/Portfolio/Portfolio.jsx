85d9efd82dfc66531d5c32583cc0c7295f75e5e7
a7med_mubarak <139309062+a7med-Mubarak@users.noreply.github.com>
2023-09-13 22:51:07 +0300

    Update Portfolio.jsx

--- app5/src/Components/Portfolio/Portfolio.jsx
+++ app5/src/Components/Portfolio/Portfolio.jsx
@@ -1,24 +1,31 @@
 
 export default function Portfolio(){
+
+
+    function imgPop(){
+        document.querySelector(".popUp").classList.replace("d-none", "d-flex");
+    }
+    function removee(){
+        document.querySelector(".popUp").classList.replace("d-flex", "d-none");
+    }
+
+
     return <>
     <section className="py-5">
         <div className="container">
 
         <h1 className="pl">PORTFOLIO COMPONENT</h1>
             <div className="mx-auto mb-3 d-flex justify-content-center">
-                <h2>_____</h2>
+                <h2>__________</h2>
                 <h2 className="pt-3">⭐</h2>
-                <h2>_____</h2>
+                <h2>__________</h2>
             </div>
 
             <div className="row g-5">
                 <div className="col-md-4 ">
-                    <div className="item">
-
-                         
-                    
-                        <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt=""/>
-                        <div className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+                    <div className="item">    
+                        <img   className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt=""/>
+                        <div className="hover" onClick={() =>imgPop()}><i className="fa-2xl fa-solid fa-plus"/></div>
                     </div>
                     
                  
@@ -27,26 +34,36 @@ export default function Portfolio(){
                 <div className="col-md-4">
                     <div className="item">
                         <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt=""/>
+                        <div onClick={() =>imgPop()} className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="item">
                         <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt=""/>
+                        <div onClick={() =>imgPop()} className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="item">
                         <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt=""/>
+                        <div onClick={() =>imgPop()} className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="item">
                         <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt=""/>
+                        <div onClick={() =>imgPop()} className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="item">
                         <img className="img-fluid" src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt=""/>
+                        <div onClick={() =>imgPop()} className="hover"><i className="fa-2xl fa-solid fa-plus"/></div>
+
                     </div>
                 </div>
             </div>
@@ -57,7 +74,7 @@ export default function Portfolio(){
 
 
 
-    <div className="popUp d-none justify-content-center align-items-center  position-fixed top-0 bottom-0 start-0 end-0 ">
+    <div onClick={removee} className="popUp d-none justify-content-center align-items-center  position-fixed top-0 bottom-0 start-0 end-0 ">
 
         <div className="child"></div>
 
@@ -68,33 +85,6 @@ export default function Portfolio(){
     </>
 }
 
-
-
-// let allImages = Array.from(document.querySelectorAll("img")) ;
-// let popup = document.querySelector(".popUp") ;
-// let child = document.querySelector(".child");
-
-// for (let i = 0; i < allImages.length; i++) {
-
-//     allImages[i].addEventListener("click" , function(eInfo){
-
-
-
-//         popup.classList.replace("d-none" , "d-flex");
-
-
-//         child.style.backgroundImage = `url(${eInfo.target.src})`;
-
-
-//     })
-    
-// }
-
-// popup.addEventListener("click", function(eInfo){
-    
-//     popup.classList.replace("d-flex" , "d-none");
-
-// })
 
 
 
